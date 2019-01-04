901ba567d98ff04fe8e2f490e725389879c96d88
romiogaku <romiogaku@me.com>
2019-01-04 21:56:16 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const recognition = new webkitSpeechRecognition();
+recognition.continuous = true;
+recognition.lang = "ja";
+recognition.onresult = event => {
+  for (let i = event.resultIndex; i < event.results.length; ++i) {
+    if (event.results[i].isFinal) {
+      const word = hiraganaToKatakana(
+        removeSpace(event.results[i][0].transcript)
+      );
+      console.log(word);
+      if (word.match(/ハローココチャン/)) {
+        console.log("ココだよ！");
+      }
+    }
+  }
+};
+recognition.error = e => {
+  console.error(e);
+};
+
+recognition.start();
+
+/**
+ * ひらがな to カタカナ
+ * @param {String} str
+ */
+const hiraganaToKatakana = str => {
+  return str.replace(/[\u3041-\u3096]/g, function(match) {
+    const chr = match.charCodeAt(0) + 0x60;
+    return String.fromCharCode(chr);
+  });
+};
+
+/**
+ * 空白除去
+ * @param {String} str
+ */
+const removeSpace = str => {
+  return str.replace(/\s+/g, "");
+};
